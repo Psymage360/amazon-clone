deffbbf24c2a803a458a09d11a3612a86f12a1f9
Ayush Chaudhary <91961830+Psymage360@users.noreply.github.com>
2023-08-01 12:15:27 +0530

    Checkout Updates

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,25 +1,51 @@
-import React from 'react'
-import './Checkout.css'
-import { useStateValue } from './StateProvider'
+import React from 'react';
+import { useStateValue} from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import './Checkout.css';
+import Subtotal from './Subtotal';
+
 
 function Checkout() {
-    const [{basket}] = useStateValue()
-  return (
-    <div className='checkout'>CheckoutME
-    {basket?.length === 0 ? (
-        <div>
-            <h2>Your Shopping Basket is empty</h2>
-            <p>You have no items in your basket. To buy one or more items, click "Add to basket" next to the item.</p>
-        </div>
-    ) : (
-        <div>
-            <h2 className='checkout__title'>Your Shopping Basket</h2>
-            {/* List out all of the Checkout Products */}
+    const [{ basket }] = useStateValue();
+    
+    return (
+        <div className="checkout">
+            <div className="checkout__left">
 
+            <img 
+                className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Pay_Balance/apay_pc_banner_2.jpg" 
+                alt=""
+            />
+            {basket?.length === 0 ? (
+                 <div>
+                    <h2>Your Amazon Cart is empty. </h2>
+                    <p>Your shopping cart is waiting. Give it purpose – fill it with groceries, clothing, household supplies, electronics and more.
+Continue shopping on the Amazon.in homepage, learn about today's deals, or visit your Wish List.</p>
+                 </div>           
+            ) : (
+                <div>
+                    <h2 className="checkout__title">Your Shopping Cart </h2>
+                    
+                    {/*List out all of the Checkout Products*/}
+                    {basket?.map(item => (
+                        <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />        
+                    ))}
+                </div>
+            )}
             </div>
-        )}
-     </div>
-  )
+            {basket.length > 0 && ( 
+            <div className="checkout__right">                
+                <Subtotal />
+            </div>
+            )}
+        </div>
+    );
 }
 
-export default Checkout
+export default Checkout;
